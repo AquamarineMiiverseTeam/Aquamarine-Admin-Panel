57d1dde89d874ff484e74c541207d7df86afbb2b
NoNameGiven <114644239+NoNameGivenCoder@users.noreply.github.com>
2024-03-04 22:12:42 -0700

    added notifications chart to analytics

--- routes/ui/analytics.js
+++ routes/ui/analytics.js
@@ -10,10 +10,13 @@ route.get("/api_calls", async (req, res) => {
     const api_calls = (await db_con.raw("SELECT DATE(create_time) AS forDate, COUNT(*) AS numAPICalls FROM api_calls GROUP BY DATE(create_time) ORDER BY forDate"))[0]
     const api_calls_num = (await db_con("api_calls").count("id"))[0]["count(`id`)"]
 
+    const api_calls_notifications = (await db_con.raw("SELECT DATE(create_time) AS forDate, COUNT(*) AS numAPICalls FROM api_calls WHERE url LIKE '%notifications%' GROUP BY DATE(create_time) ORDER BY forDate"))[0]
+
     res.render("analytics/api.ejs", {
         account : req.account[0],
         api_calls : api_calls,
-        api_calls_num : api_calls_num
+        api_calls_num : api_calls_num,
+        api_calls_notifications : api_calls_notifications
     })
 })
 
--- static/js/analytics.js
+++ static/js/analytics.js
@@ -1,5 +1,6 @@
 async function init_data() {
     let data = JSON.parse(document.querySelector("canvas[data-table-data]").getAttribute("data-table-data"))
+    let notifications_data = JSON.parse(document.querySelector("canvas[data-table-data]").getAttribute("data-table-notifications-data"))
 
     let chart_data = [];
     let labels = [];
@@ -12,6 +13,17 @@ async function init_data() {
         chart_data.push({x : new Date(e.forDate).toLocaleDateString(), y : e.numAPICalls})
     })
 
+    let chart_data_notifications = [];
+    let labels_notifications = [];
+
+    notifications_data.forEach((e) => {
+        labels_notifications.push(new Date(e.forDate).toLocaleDateString())
+    })
+
+    notifications_data.forEach((e) => {
+        chart_data_notifications.push({x : new Date(e.forDate).toLocaleDateString(), y : e.numAPICalls})
+    })
+
     const chart = new Chart("api_usage_chart", {
         type: "line",
         data: {
@@ -22,7 +34,15 @@ async function init_data() {
                 borderColor : "blue",
                 fill: false,
                 tension: 0
-            }]
+            },
+            {
+                label : "Notifications",
+                data : chart_data_notifications,
+                borderColor : "pink",
+                fill: false,
+                tension: 0
+            }
+        ]
         },
         options: {}
     })
